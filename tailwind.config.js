e16d2bae25bda5c957c1d0a4017bc5c61ae4bd66
atharvaupare <atharvaupare5@gmail.com>
2023-09-22 15:36:06 +0530

    feat: prisoner page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat",
+        inter: "Inter",
+      },
+      colors: {
+        white: "#fff",
+        lightseagreen: "#41c0ab",
+        black: "#000",
+        silver: "#b4bec9",
+        whitesmoke: "#efefef",
+        gray: "#010101",
+      },
+    },
   },
   plugins: [],
 }
