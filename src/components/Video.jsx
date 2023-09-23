cf4647bb9368abd8d6cf80a45510f5f20dccfee0
Jiya Trivedi <jiyatrivedi204@gmail.com>
2023-09-23 22:32:41 +0530

    refact:video iframes fixed

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ function Video({title,link}) {
         
       <p className=" font-medium text-black text-base mb-[0.6rem]">{title} :</p>
       <div className="rounded-2xl shadow-lg overflow-hidden">
-      <iframe width="300" height="200" src={link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className='w-full' height="200" src={link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     
     </div>
