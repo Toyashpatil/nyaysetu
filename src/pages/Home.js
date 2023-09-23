365a7bc4761c7998923b279d65c982912036879e
Toyash <toyashpatil17@gmail.com>
2023-09-23 09:59:36 +0530

    fix: bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,6 +23,7 @@ const Home = () => {
             )
           })
       }else{
+        console.log(name)
         setAuth((prev)=>{
           return(
             {
@@ -69,27 +70,27 @@ const Home = () => {
         <p className='text-[1.5rem] not-italic font-semibold leading-[normal]'>Welcome</p>
         <form className='inputs  h-auto w-[80%] mt-4 inline-flex flex-col  items-start gap-6'>
           <div className='input1 w-full flex flex-col items-start gap-1.5'>
-            <p className='text-[1rem] not-italic font-medium leading-[normal]'>Username:</p>
-            <input placeholder='username' name='username' className='username lex w-[100%] items-center border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] px-[0.3125rem] py-2 rounded-[0.3.25rem] border-solid border-[#41C0AB] bg-[#efefef]'></input>
+            <p className='text-[1rem] not-italic font-medium leading-[normal]' >Username:</p>
+            <input placeholder='username' className='username lex w-[100%] items-center border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] px-[0.3125rem] py-2 rounded-[0.3.25rem] border-solid border-[#41C0AB] bg-[#efefef]'onChange={handleChange} name="name"></input>
           </div>
           <div className=' input2 w-full flex flex-col justify-center items-start gap-[0.3rem]'>
             <p className='text-[1rem] not-italic font-medium leading-[normal]'>Designation:</p>
             <div className='flex items-center gap-12'>
               <div className='forLawyer flex items-center gap-[0.56rem]'>
                 <p className='text-[1rem] not-italic font-medium leading-[normal]'>Lawyer</p>
-                <input className="lawyer border rounded-sm border-solid border-[#41C0AB]" type='checkbox' ></input>
+                <input className="lawyer border rounded-sm border-solid border-[#41C0AB]" type='checkbox' onChange={handleChange} name='lawyer' ></input>
               </div>
               <div className=' forUtp flex items-center gap-[0.9rem]'>
-                <p className='text-[1rem] not-italic font-medium leading-[normal]'>Undertrial Prisoner</p>
+                <p className='text-[1rem] not-italic font-medium leading-[normal]' onChange={handleChange} name='prisoner' >Undertrial Prisoner</p>
                 <input type='checkbox'className='utp border rounded-sm border-solid border-[#41C0AB]' ></input>
               </div> 
             </div>
           </div>
           <div className='input3 w-full flex flex-col items-start gap-1.5'>
           <p className='text-[1rem] not-italic font-medium leading-[normal]'>Enter CNR/Enrollment Number:</p>
-          <input className="number flex w-[100%] items-center px-[0.3125rem] py-2 border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] rounded-[0.325rem] border-solid border-[#41C0AB] bg-[#efefef]" placeholder='CNR/Enrollment Number' ></input>
+          <input className="number flex w-[100%] items-center px-[0.3125rem] py-2 border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] rounded-[0.325rem] border-solid border-[#41C0AB] bg-[#efefef]" placeholder='CNR/Enrollment Number' onChange={handleChange} name='veriNum' ></input>
           </div>
-          <button type="submit" className='flex w-[100%] justify-center items-center px-[0.3125rem] py-2  border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] rounded-[0.3125rem] border-solid border-[#41C0AB] bg-[#41C0AB] '>Continue</button>
+          <button type="submit" className='flex w-[100%] justify-center items-center px-[0.3125rem] py-2  border shadow-[0px_4px_4px_0px_rgba(0,0,0,0.10)] rounded-[0.3125rem] border-solid border-[#41C0AB] bg-[#41C0AB] ' onClick={handleClick}>Continue</button>
 
         </form>
         
