16869b07b2b6945aa83efba748cbab92d495ac24
Jiya Trivedi <jiyatrivedi204@gmail.com>
2023-09-23 11:34:30 +0530

    feat:Monitor cases page

---
+++ src/components/Live.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import RectangleImg from "../pages/assets/rectangleimg.jpg"
+function live() {
+  return (
+    <div>
+        <div className="relative my-10 mx-6">
+    <div className="border border-lightseagreen inset rounded-2xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.15)] overflow-hidden">
+        <img className="w-full h-32 object-cover opacity-[0.10] " src={RectangleImg}></img>
+    </div>
+    <div className="absolute top-0 left-0 p-4">
+        <div className="flex flex-col gap-2">
+            <div className="text-base font-bold text-black">CNR Number, </div>
+            <div className="text-lightseagreen font-semibold text-2xl">XXXX XXXX XXXX XXXX</div>   
+            <div className="flex flex-row gap-2">
+                <span className="text-black font-bold">Case Status: </span>
+                <span className="text-lightseagreen font-semibold">Pending </span>
+            </div>
+        </div> 
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default live
