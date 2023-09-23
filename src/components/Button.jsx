6316b24e993134c7cfe33b66f4b8903bba538617
atharvaupare <atharvaupare5@gmail.com>
2023-09-23 22:18:53 +0530

    fix: component fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import AuthContext from "../context/authContext";
 const Button = ({className, children, onClick}) => {
   return (
     <>
-      <button onClick={onClick} className={`text-base text-white font-medium bg-lightseagreen rounded-md flex items-center justify-center ${className}`}>
+      <button onClick={onClick} className={`text-base text-white font-medium bg-lightseagreen rounded-md flex items-center justify-center shadow-lg ${className}`}>
         {children}
       </button>
     </>
